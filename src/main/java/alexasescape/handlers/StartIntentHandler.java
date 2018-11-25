c47ff1a16043bf1900a15f812b4c02ce8c0e2d7c
Julian T <julian.taxis74@gmail.com>
2018-11-25 12:43:32 +0100

    Added Highscore- and StartIntenthandler

--- src/main/java/alexasescape/handlers/StartIntentHandler.java
+++ src/main/java/alexasescape/handlers/StartIntentHandler.java
@@ -1,11 +1,8 @@
 /*
      Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
      Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
      except in compliance with the License. A copy of the License is located at
-
          http://aws.amazon.com/apache2.0/
-
      or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
      the specific language governing permissions and limitations under the License.
@@ -15,29 +12,28 @@ package alexasescape.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
 
 import java.util.Collections;
 import java.util.Optional;
 
-import static com.amazon.ask.request.Predicates.requestType;
+import static com.amazon.ask.request.Predicates.intentName;
 
-public class LaunchRequestHandler implements RequestHandler {
+public class StartIntentHandler implements RequestHandler {
     @Override
     public boolean canHandle(HandlerInput input) {
-        return input.matches(requestType(LaunchRequest.class));
+        return input.matches(intentName("StartIntent"));
     }
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        String speechText = "Hallo. Moechtest Du das Spiel starten oder Deinen Highscore abfragen";
-        String reprompt = "Starte das Spiel oder frage Deinen Highscore ab.";
+        //Telefongeläute
+        String speechText = "hier kommt eine krasse Story";
         input.getAttributesManager().setSessionAttributes(Collections.singletonMap("Reprompt", speechText));
         return input.getResponseBuilder()
-                .withSimpleCard("Alexas Escape Menu", speechText)
+                .withSimpleCard("Alexas Escape started", speechText)
                 .withSpeech(speechText)
-                .withReprompt(reprompt)
+                .withShouldEndSession(false)
                 .build();
     }
 }
