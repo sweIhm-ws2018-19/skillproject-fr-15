669c4759337ff733467d58501dfb3fe6b89c3d51
Lenz-K <lkarbaum@hm.edu>
2018-11-26 21:49:24 +0100

    fixed class HangUpIntent

--- src/main/java/alexasescape/handlers/HangUpIntentHandler.java
+++ src/main/java/alexasescape/handlers/HangUpIntentHandler.java
@@ -2,18 +2,17 @@ package alexasescape.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
 
 import java.util.Optional;
 
 import static alexasescape.handlers.RepeatIntentHandler.REPROMPT_KEY;
-import static com.amazon.ask.request.Predicates.requestType;
+import static com.amazon.ask.request.Predicates.intentName;
 
 public class HangUpIntentHandler implements RequestHandler {
     @Override
     public boolean canHandle(HandlerInput input) {
-        return input.matches(requestType(LaunchRequest.class));
+        return input.matches(intentName("HangUpIntent"));
     }
 
     @Override
