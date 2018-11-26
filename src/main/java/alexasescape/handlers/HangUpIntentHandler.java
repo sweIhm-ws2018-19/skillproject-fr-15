a679645d328fad3f1131533a7dd468163a38cae5
Lenz-K <lkarbaum@hm.edu>
2018-11-26 20:49:24 +0100

    only HUIH

---
+++ src/main/java/alexasescape/handlers/HangUpIntentHandler.java
@@ -0,0 +1,31 @@
+
+
+package alexasescape.handlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class HangUpIntentHandler implements RequestHandler {
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(intentName("HangUpIntent"));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "Hallo. Moechtest Du das Spiel starten oder Deinen Highscore abfragen";
+        String reprompt = "Starte das Spiel oder frage Deinen Highscore ab.";
+        input.getAttributesManager().setSessionAttributes(Collections.singletonMap("Reprompt", speechText));
+        return input.getResponseBuilder()
+                .withSimpleCard("Alexas Escape Menu", speechText)
+                .withSpeech(speechText)
+                .withReprompt(reprompt)
+                .build();
+    }
+}
