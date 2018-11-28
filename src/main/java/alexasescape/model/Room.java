eece2d0342215c1fd1d731c46f2edc4cc6123d14
Julian <julian.taxis@hm.edu>
2018-11-28 15:03:51 +0100

    added Unit Tests for Room

--- src/main/java/alexasescape/model/Room.java
+++ src/main/java/alexasescape/model/Room.java
@@ -9,6 +9,9 @@ public class Room {
     private List<Item> items;
 
     public Room(String name, List<Item> items) {
+        Objects.requireNonNull(items,"Items must not be null");
+        if(items.isEmpty() || items.size() > 4)
+            throw new IllegalArgumentException("Room must contain between 1 and 4 items");
         this.name = name;
         this.items = items;
     }
---
+++ src/test/java/alexasescape/model/RoomTest.java
@@ -0,0 +1,66 @@
+package alexasescape.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoomTest {
+    private Room room;
+    private List<Item> items;
+
+    @Before
+    public void setUP(){
+        items = new ArrayList<>();
+        items.add(new Item("test", true));
+        room = new Room("test", items);
+    }
+    @Test(expected = NullPointerException.class)
+    public void testNoItems(){
+        new Room("Test", new ArrayList<>(null));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTooMuchItems(){
+        List<Item> itemsTest = new ArrayList<>();
+        items.add(new Item("test", true));
+        items.add(new Item("test1", true));
+        items.add(new Item("test2", true));
+        items.add(new Item("test3", true));
+        items.add(new Item("test4", true));
+        new Room("Test", items);
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("test", room.getName());
+    }
+
+    @Test
+    public void testGetItems() {
+        assertEquals(items, room.getItems());
+    }
+
+    @Test
+    public void testEquals() {
+        final Room secondRoom = new Room("test", items);
+        assertEquals(room,secondRoom);
+    }
+    @Test
+    public void testEqualsSame() {
+        assertEquals(room,room);
+    }
+    @Test
+    public void testEqualsNull() {
+        assertEquals(room,null);
+    }
+
+    @Test
+    public void testHashCode() {
+        final Room secondRoom = new Room("test", items);
+        assertEquals(room.hashCode(), secondRoom.hashCode());
+    }
+}
