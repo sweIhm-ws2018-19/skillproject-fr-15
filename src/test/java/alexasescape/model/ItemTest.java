f15bb23c0a6d1f8f7bb1582568489c331275df7e
Daniel Seifert <seifert-daniel1@gmx.de>
2018-11-28 14:22:23 +0100

    Added tests for Item

---
+++ src/test/java/alexasescape/model/ItemTest.java
@@ -0,0 +1,47 @@
+package alexasescape.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    private Item item;
+
+    @Before
+    public void setUp() {
+        item = new Item("Item", true);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testUse() {
+        item.use();
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Item", item.getName());
+    }
+
+    @Test
+    public void testIsUsable() {
+        assertTrue(item.isUsable());
+    }
+
+    @Test
+    public void setUsable() {
+        item.setUsable(false);
+        assertFalse(item.isUsable());
+    }
+
+    @Test
+    public void testEquals() {
+        assertEquals(new Item("Item", true), item);
+    }
+
+    @Test
+    public void testHashCode() {
+        assertEquals(new Item("Item", true).hashCode(), item.hashCode());
+    }
+}
