161d1e5c0bce3ab0fefac3f562baed2c2d464f96
Julian <julian.taxis@hm.edu>
2018-12-01 14:24:33 +0100

    Updated Game tests

--- src/test/java/alexasescape/model/GameTest.java
+++ src/test/java/alexasescape/model/GameTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 import static org.junit.Assert.*;
 
@@ -19,22 +18,22 @@ public class GameTest {
         List<Room> rooms = new LinkedList<>();
         final int maxAttempts = 5;
         List<Item> items = new ArrayList<>();
-        items.add(new Item("Test", false));
+        items.add(new Item("Test", "xy",false));
         rooms.add(new Room("Room", items));
         rooms.add(new Room("Room", items));
         rooms.add(new Room("Room", items));
-        game = new Game(maxAttempts,rooms, new Player("test"));
+        game = new Game(maxAttempts,rooms, new Player("test", new Highscore()));
     }
 
 
     @Test(expected = NullPointerException.class)
     public void testConstructorWithNullAsRoom() {
-        new Game(5,null, new Player("test"));
+        new Game(5,null, new Player("test", new Highscore()));
 }
 
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorWithEmptyRooms() {
-        new Game(5, new ArrayList<>(), new Player("test"));
+        new Game(5, new ArrayList<>(), new Player("test", new Highscore()));
     }
 
 
